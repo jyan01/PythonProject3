d1673aa9fd524f436a180fa4f969e6ef7eddc495
jyan01 <jyan02@naver.com>
2018-03-24 09:18:00 +0900

    PythonProject3.py

---
+++ PythonProject3.py
@@ -0,0 +1,124 @@
+print(len("가나다라마바사아자차카타파하"))
+print(type("안녕하세요"))
+print(type(len("안녕하세요")))
+
+ 
+
+print(273)
+print(52.273)
+print(type(273))
+print(type(52.273))
+ 
+
+print(0)
+print(0.0)
+print(type(0))
+print(type(0.0))
+
+ 
+
+print(5 + 7)
+print(5 - 7)
+print(5 * 7)
+print(5 / 7)
+print(5 // 7)
+print(5 % 2)
+
+# 출력합니다.
+
+
+print("# 제곱 연산자")
+print("2 ** 1 =", 2 ** 1)
+print("2 ** 2 =", 2 ** 2)
+print("2 ** 3 =", 2 ** 3)
+print("2 ** 4 =", 2 ** 4)
+
+ 
+
+print("2 + 2 - 2 * 2 / 2 * 2 =", 2 + 2 - 2 * 2 / 2 * 2)
+print("2 - 2 + 2 / 2 * 2 + 2 =", 2 - 2 + 2 / 2 * 2 + 2)
+
+ 
+
+print("안녕" + "하세요" * 3)
+
+ 
+
+string = "문자열" #오류
+number = 273 #오류
+string + number #오류
+
+ 
+
+pi = 3.14159265
+print(pi)
+
+ 
+
+# 숫자 복합 대입 연산자
+number = 100
+number += 10
+number += 20
+number += 30
+print("number:", number)
+
+ 
+
+# 문자열 복합 대입 연산자
+string = "안녕하세요"
+string += "!"
+string += "!"
+print("string:", string)
+
+ 
+
+# 입력을 받습니다.
+string = input("입력> ")
+
+ 
+
+# 출력합니다.
+print(string)
+
+ 
+
+# 입력을 받습니다.
+string = input("입력> ")
+
+ 
+
+# 출력합니다.
+print("입력:", string)
+print("자료형:", type(string))
+
+ 
+
+# 입력을 받습니다.
+string = input("입력> ")
+
+# 출력합니다.
+
+print("입력 + 100", string + 100) #오류
+
+ 
+
+input_a = float(input("첫 번째 숫자> "))
+input_b = float(input("두 번째 숫자> "))
+
+ 
+
+print("덧셈 결과:", input_a + input_b)
+print("뺄셈 결과:", input_a - input_b)
+print("곱셈 결과:", input_a * input_b)
+print("나눗셈 결과:", input_a / input_b)
+
+ 
+
+output_a = str(52)
+output_b = str(52.237)
+
+ 
+
+print(type(output_a), output_a)
+print(type(output_b), output_b)
+ 
